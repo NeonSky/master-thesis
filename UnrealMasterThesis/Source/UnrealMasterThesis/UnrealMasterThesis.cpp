afdf528f695b5f5cddd7f601e654ea7ad009fdb2
Jacob Eriksson <jacob.a.eriksson@gmail.com>
2022-01-17 15:15:42 +0100

    Empty unreal project

---
+++ UnrealMasterThesis/Source/UnrealMasterThesis/UnrealMasterThesis.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealMasterThesis.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealMasterThesis, "UnrealMasterThesis" );
---
+++ UnrealMasterThesis/Source/UnrealMasterThesis/UnrealMasterThesisGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealMasterThesisGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALMASTERTHESIS_API AUnrealMasterThesisGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
