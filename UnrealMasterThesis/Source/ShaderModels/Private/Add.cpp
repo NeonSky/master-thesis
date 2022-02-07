f8f20f31eb56ae6bfc31f6066cf035e95f29b00d
Jacob Eriksson <jacob.a.eriksson@gmail.com>
2022-02-07 12:51:34 +0100

    Works for 8x8 as well

--- UnrealMasterThesis/Source/ShaderModels/Private/Add.cpp
+++ UnrealMasterThesis/Source/ShaderModels/Private/Add.cpp
@@ -10,7 +10,7 @@
 #include <random>
 
 #define NN 256
-#define TEMP_TEXTURE_N 4
+#define TEMP_TEXTURE_N 8
 
 IMPLEMENT_GLOBAL_SHADER(AddShader, "/Project/UnrealMasterThesis/Add.usf", "eWaveCompute", SF_Compute);
 
