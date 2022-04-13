2c48759c0d7b6ef7022a084edca39de5bb272e8e
Joakim <joakim@wingard.nu>
2022-04-13 14:18:13 +0200

    Python scripts for comparing consistency and creating position difference plot + path plot

---
+++ PythonDataAnalysis/CompareFiles.py
@@ -0,0 +1,50 @@
+import os
+
+directory = '../UnrealMasterThesis/SavedBoatData/Pre_PR_Test'
+
+filePaths = []
+numFiles = 0
+files = {}
+for filename in os.listdir(directory):
+    fpath = os.path.join(directory, filename)
+    numFiles += 1
+    file = open(fpath)
+    files[fpath] = file.readlines()
+    file.close()
+
+scores = {}
+for path_1, contents_1 in files.items():
+    if path_1 not in scores:
+        scores[path_1] = 0
+
+    for path_2, contents_2 in files.items():
+        if path_1 == path_2:
+            continue
+        if path_2 not in scores:
+            scores[path_2] = 0
+
+        if contents_1 == contents_2:
+            scores[path_1] += 1
+
+consistentReferenceFile = None
+inconsistentFiles = []
+for file, score in scores.items():
+    if score == 0:
+        print(file + ' is not matching any other files! INCONSISTENCY!')
+        inconsistentFiles.append(file)
+    else:
+        if consistentReferenceFile == None:
+            consistentReferenceFile = files[file]
+        print(file + ' is matching ' + str(score) + ' other files.')
+
+for file in inconsistentFiles:
+    lineNr = 0
+    for line in files[file]:
+        refLine = consistentReferenceFile[lineNr]
+        if line != refLine:
+            print('------------------------------------------\n' + file + ' has its first inconsistency at line ' + str(lineNr))
+            print('inconsistent line: ' + line)
+            print('reference line: ' + refLine)
+            break
+        lineNr += 1
+
