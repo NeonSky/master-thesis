5b12f4acb1c6fbd0472797a309fbe089332cfe14
Joakim <joakim@wingard.nu>
2022-04-15 11:18:29 +0200

    Plot script now handles either multiple types (one player) or multiple simultaneous players.

--- PythonDataAnalysis/PositionPlots.py
+++ PythonDataAnalysis/PositionPlots.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 from matplotlib import pyplot as plt
 
-dir = '../UnrealMasterThesis/SavedBoatData/TwoBoats_2'
+dir = '../UnrealMasterThesis/SavedBoatData/TwoBoats_2_x1'
 
 # Set 'dir' to a folder with recorded boat data
 # If more than one recording per boat type is present, only the first file for each type will be used
@@ -11,24 +11,25 @@ dir = '../UnrealMasterThesis/SavedBoatData/TwoBoats_2'
 
 
 def extractPositionData(in_json_list):
-    boat_positions = []
+    result = []
     for frameData_str in in_json_list:
         frameData = json.loads(frameData_str)
         pos = np.array([frameData['position_x'],
                          frameData['position_y'],
                          frameData['position_z']])
-        boat_positions.append(pos)
-    return boat_positions
+        result.append(pos)
+    return result
 
 
 def getErrorMagnitudes(referece_data, other_data):
     error_magnitudes = []
-    for i in range(len(referece_data)):
-        b1 = referece_data[i]
-        b2 = other_data[i]
-        diff = b1-b2
-        mag = np.sqrt(diff.dot(diff))
-        error_magnitudes.append(mag)
+    if not len(other_data) == 0:
+        for i in range(len(referece_data)):
+            b1 = referece_data[i]
+            b2 = other_data[i]
+            diff = b1-b2
+            mag = np.sqrt(diff.dot(diff))
+            error_magnitudes.append(mag)
     return error_magnitudes
 
 
@@ -81,111 +82,76 @@ def getJSONdataFromAllFilesInDir(directory):
         file.close()
     return json_data
 
-def createPathsPlot(title):
+def createPathsPlot(title, pos_data):
     # Plot boat travel paths for 2 boats
     plt.title(title)
     plt.xlabel("x")
     plt.ylabel("y")
-    position_data_2D = {}
-    position_data_2D_2 = {}
-    for boat_type, positions in position_data.items():
-        position_data_2D[boat_type] = np.array([[x, y] for (x, y, z) in positions])
-
-    for boat_type, positions_2D in position_data_2D.items():
-        plt.plot(positions_2D[:, 0], positions_2D[:, 1], label=boat_type)
-
-    for boat_type, positions in position_data_2.items():
-        position_data_2D_2[boat_type] = np.array([[x, y] for (x, y, z) in positions])
-
-    for boat_type, positions_2D in position_data_2D_2.items():
-        plt.plot(positions_2D[:, 0], positions_2D[:, 1], label=boat_type)
-
     plt.grid(visible=True)
-    plotLimits = getPlotLimits(position_data)
+    plotLimits = getPlotLimits(position_data)  # TODO: it would be nice if the limits considered simultaneous boats.
     plt.xlim(plotLimits[0], plotLimits[1])
     plt.ylim(plotLimits[2], plotLimits[3])
+    for b in pos_data:
+        positions_2D = {}
+        for boat_type, positions_3D in b.items():
+            if len(positions_3D) > 0:
+                positions_2D[boat_type] = np.array([[x, y] for (x, y, z) in positions_3D])
+        for lbl, positions in positions_2D.items():
+            plt.plot(positions[:, 0], positions[:, 1], label=lbl)
     plt.legend(loc="upper left")
     plt.show()
+
+def createDifferenceMagnitudePlot(title, reference, difference_magnitudes, numFrames = 600): # TODO 600
+    x = np.arange(0, numFrames)
+    plt.title(title)
+    plt.xlabel("Frame")
+    plt.ylabel("Position difference magnitude")
+    for boat_type, diff in difference_magnitudes.items():
+        if len(diff) > 0:
+            plot_label = reference + ' vs ' + boat_type
+            plt.plot(x, diff, label=plot_label)
+    plt.legend(loc="upper left")
+    plt.show()
+
 
 
 if __name__ == '__main__':
     json_data = getJSONdataFromAllFilesInDir(dir)
-
-    #print(json_data.items())
     # Extract Position data into arrays
+    boat_1 = boat_2 = boat_3 = False
     position_data = {}
     position_data_2 = {}
     position_data_3 = {}
+    numFrames = 0
     for boat_type, boats in json_data.items():
-        print(boats['boat_0'])
         position_data[boat_type] = extractPositionData(boats['boat_0'])
         position_data_2[boat_type] = extractPositionData(boats['boat_1'])
         position_data_3[boat_type] = extractPositionData(boats['boat_2'])
+        numFrames = len(position_data[boat_type])
+        if len(position_data_2[boat_type]) > 0:
+            boat_2 = True
+        if len(position_data_3[boat_type]) > 0:
+            boat_3 = True
 
-    # Get magnitude or diff compared to GPU boat
-    diff_magnitudes = {}
-
-    # GPU boat is the reference boat we compare the difference with
-    reference_positions = list(position_data.values())[0]
-    if 'GPU' in position_data:
+    if not boat_2 and not boat_3:
+        # The data in this folder only contains one simultaneous boat.
+        # Assuming a plot of different boat types is of interest.
+        diff_magnitudes = {}
         reference_positions = position_data['GPU']
-
-    for boat_type, positions in position_data.items():
-        if boat_type == 'GPU':
-            continue
-        diff_magnitudes[boat_type] = getErrorMagnitudes(reference_positions, positions)
-
-    # Plot difference magnitudes
-    x = np.arange(0, 600)
-    plt.title("Boat Position Difference")
-    plt.xlabel("frame")
-    plt.ylabel("difference magnitudes")
-    for boat_type, diff in diff_magnitudes.items():
-        plot_label = 'GPU vs ' + boat_type
-        plt.plot(x, diff, label=plot_label)
-    plt.legend(loc="upper left")
-    plt.show()
-
-    # Plot boat travel paths
-    plt.title("Boat Paths")
-    plt.xlabel("x")
-    plt.ylabel("y")
-    position_data_2D = {}
-    for boat_type, positions in position_data.items():
-        position_data_2D[boat_type] = np.array([[x, y] for (x, y, z) in positions])
-
-    for boat_type, positions_2D in position_data_2D.items():
-        plt.plot(positions_2D[:, 0], positions_2D[:, 1], label=boat_type)
-
-    plt.grid(visible=True)
-    plotLimits = getPlotLimits(position_data)
-    plt.xlim(plotLimits[0], plotLimits[1])
-    plt.ylim(plotLimits[2], plotLimits[3])
-    plt.legend(loc="upper left")
-    plt.show()
-
-    if True:
-        # Plot boat travel paths for 2 boats
-        plt.title("Boat Paths - 2 boats")
-        plt.xlabel("x")
-        plt.ylabel("y")
-        position_data_2D = {}
-        position_data_2D_2 = {}
         for boat_type, positions in position_data.items():
-            position_data_2D[boat_type] = np.array([[x, y] for (x, y, z) in positions])
-
-        for boat_type, positions_2D in position_data_2D.items():
-            plt.plot(positions_2D[:, 0], positions_2D[:, 1], label=boat_type)
-
-        for boat_type, positions in position_data_2.items():
-            position_data_2D_2[boat_type] = np.array([[x, y] for (x, y, z) in positions])
-
-        for boat_type, positions_2D in position_data_2D_2.items():
-            plt.plot(positions_2D[:, 0], positions_2D[:, 1], label=boat_type)
-
-        plt.grid(visible=True)
-        plotLimits = getPlotLimits(position_data)
-        plt.xlim(plotLimits[0], plotLimits[1])
-        plt.ylim(plotLimits[2], plotLimits[3])
-        plt.legend(loc="upper left")
-        plt.show()
+            if boat_type == 'GPU':  # GPU is the reference boat, no need to compare GPU vs GPU.
+                continue
+            diff_magnitudes[boat_type] = getErrorMagnitudes(reference_positions, positions)
+        createDifferenceMagnitudePlot("Error, different boat types", "GPU", diff_magnitudes, numFrames)
+        createPathsPlot("One player, multiple boat types", [position_data])
+    else:
+        # At least 2 simultaneous boats.
+        # Assuming a plot of comparing simultaneous boats is of interest
+        diff_magnitudes = {}
+        reference_positions = list(position_data.values())[0]  # reference boat is boat 1
+        boat_2_positions = list(position_data_2.values())[0]
+        boat_3_positions = list(position_data_3.values())[0]
+        diff_magnitudes['Boat 2'] = getErrorMagnitudes(reference_positions, boat_2_positions)
+        diff_magnitudes['Boat 3'] = getErrorMagnitudes(reference_positions, boat_3_positions)
+        createDifferenceMagnitudePlot("Error, simultaneous boats", "Boat 1", diff_magnitudes, numFrames)
+        createPathsPlot("Paths, multiple simultaneous boats", [position_data, position_data_2, position_data_3])
